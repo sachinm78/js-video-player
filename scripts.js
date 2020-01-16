2758be819e30277cd619dbc56eefc4eefb2496e0
sachinm78 <sachinm78@gmail.com>
2020-01-16 16:04:33 -0600

    added updateButton function and event listeners to toggle button icon

--- scripts.js
+++ scripts.js
@@ -21,12 +21,18 @@ function togglePlay() {
 //     const method = video.paused ? 'play' : 'pause';
 //     video[method]();
 // }
-
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
 
 
 
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
